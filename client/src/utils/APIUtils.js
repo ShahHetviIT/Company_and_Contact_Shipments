fd1d00884e56f95629de10dcafa8f673ea62d098
ShahHetviIT <hetvi4551@gmail.com>
2024-07-01 16:35:00 +0530

    host url changed to backend live url

--- client/src/utils/APIUtils.js
+++ client/src/utils/APIUtils.js
@@ -1,3 +1,4 @@
-export const host = "http://localhost:3001";
+// export const host = "http://localhost:3001";
+export const host = "https://company-and-contact-shipments.onrender.com";
 export const addCompanyDetailsRoute = `${host}/api/company/addCompayDetails`;
 export const addContactDetailsRoute = `${host}/api/contact/addContactDetails`;
